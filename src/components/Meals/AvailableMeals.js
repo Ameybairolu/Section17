d4fdb8a5c3960fac72c0fc02c2e89a33b5f4b3f8
Ameybairolu <ameybairolu@gmail.com>
2021-12-12 18:56:08 +0530

    Fetched data from the firebase and rendered

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,17 +1,36 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
-import { useEffect } from 'react';
 
 
 const AvailableMeals = () => {
+
+  const [meals, setMeals] = useState([]);
+
   useEffect(() => {
-    fetch('https://seventeen-meals-default-rtdb.firebaseio.com/meals.json');
+    const fetchMeals = async () => {
+      const response = await fetch('https://seventeen-meals-default-rtdb.firebaseio.com/meals.json');
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price
+        })
+      }
+
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
   }, []);
 
 
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
